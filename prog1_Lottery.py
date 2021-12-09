bc0177649a24e8a8a46e7e46f7f70b1b58fcb23b
Jelen Kriz Angel Mampusti <92630388+jelenkrizangel26@users.noreply.github.com>
2021-12-09 23:32:05 +0800

    Done asking whether to play or not

--- prog1_Lottery.py
+++ prog1_Lottery.py
@@ -18,9 +18,23 @@ win_price = 5,550,000
 def intro():
     print("\n\t\33[1m\33[35m     Welcome to Krizzy's Lottery\33[0m")
     print("\t\33[3mGet a chance to win 5,550,000 pesos!\33[0m")
-    name = input("\nKindly, enter your name: ")
+    name = input("\nKindly, enter your name\33[1m: ")
     print(f"\n\t\33[1m\33[35m            Hello {name.capitalize()}\U0001F601\33[0m")
-    print("\t\33[3m      Are you ready win prizes?\33[0m")
+    print("\t\33[3m     Are you ready win prizes?\33[0m")
 
-    
+def readyXnot():
+    print("\n\t\33[1m Kindly, type \33[35my\33[0m \33[1mif yes and \33[35mn\33[0m \33[1mif no.\33[0m")
+    response = input(">>\33[1m ")
+    print("\33[0m")
+    if response.lower() == "y":
+        print("\t\33[1m       Pick wisely! Goodluck\33[0m\U0001F609")
+    elif response.lower() == "n":
+        print("\n   Thank you! Come back again where you're ready!")
+        print("\t       You may now exit\U0001F607")
+        sys.exit("\n")
+    else:
+        print("\33[31m\33[3m\33[1m  Sorry you it looks like you type an invalid keyword!\33[0m")
+        return readyXnot()
+
 intro()
+readyXnot()
